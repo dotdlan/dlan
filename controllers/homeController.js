754ba5e1b46c5822741b37cb31e985a427eb2fc9
David Lance <david6116@yahoo.com>
2020-07-20 15:11:42 -0400

    add bio, project, resume ejs and route. add basic index content

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,4 +5,16 @@ router.get('/', (req, res) =>{
     res.render('pages/index.ejs')
 })
 
+router.get('/resume', (req, res) =>{
+    res.render('pages/resume.ejs')
+})
+
+router.get('/bio', (req, res) =>{
+    res.render('pages/bio.ejs')
+})
+
+router.get('/projects', (req, res) =>{
+    res.render('pages/projects.ejs')
+})
+
 module.exports = router;
